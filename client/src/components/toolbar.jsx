0eda69b60d614918382e8c42d8429be35f76729d
Brett van Beynum <maildrop444@gmail.com>
2022-02-04 10:08:48 -0500

    Fixed padding when no back button

--- client/src/components/toolbar.jsx
+++ client/src/components/toolbar.jsx
@@ -15,16 +15,16 @@ const Toolbar = (props) => {
 
 	return (
 		<div className="toolbar">
+			<div className="toolbarButton">
 			{
 			props.teamName ?
-			<div className="toolbarButton" onClick={ () => { props.navBack() } }>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={ () => { props.navBack() } }>
 					<path d="M0 0h24v24H0z" fill="none"/>
 					<path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" fill="black"/>
-				</svg>
-			</div>
-			: ""
+				</svg>		
+				: ""
 			}
+			</div>
 
 			<h2 className="toolbarHeader">Game Day { props.teamName ? " - " + props.teamName : "" }</h2>
 			
